c01dc9cbe8cf7163c5ffc8fa98369126f0fa9a3a
William Moss <wckmoss@gmail.com>
2021-03-01 11:00:06 -0500

    Updated client.py and server.py
    I updated the server and client code for the Tic-Tac-Toe game

--- client.py
+++ client.py
@@ -1,8 +1,8 @@
-    from socket import *
+from socket import *
 import os 
 
 # Server IP Address and Port
-HOST = "18.219.39.168"
+HOST = "localhost"
 PORT = 22
 BUFFER_SIZE = 1024
 
@@ -19,21 +19,72 @@ try:
     playerName = input("Please enter your name: ")
     clientSocket.sendall(playerName.encode())
     
+    # Receive scoreboard
+    curr_scoreboard = clientSocket.recv(BUFFER_SIZE).decode()
+    print(curr_scoreboard)
     
-    """
     while True:
-        response = clientSocket.recv(BUFFER_SIZE).decode()
-        print(response)
-        data = input()
-        clientSocket.sendall(data.encode)
-    """
+        while True:
+            # Receive current board 
+            curr_board = clientSocket.recv(BUFFER_SIZE).decode()
+            print(curr_board)
+            
+            # Check if this is the player's move (1 for yes, 0 for no)
+            turn_checker = clientSocket.recv(BUFFER_SIZE).decode()
+            
+            if (turn_checker == "0"):
+                # Wait for other player
+                print("Waiting for other player's move...")
+            elif (turn_checker == "1"):
+                while True:
+                    try: 
+                        # Get player move 
+                        player_move = input("Your turn! Which box : ")
+                        # Checks if player_move is a number
+                        if (int(player_move) < 1 or int(player_move) > 9):
+                            print("Wrong Input! Try again")
+                        else:
+                            clientSocket.sendall(player_move.encode())
+                            # Check if box is not occupied
+                            fill_checker = clientSocket.recv(BUFFER_SIZE).decode()
+                            if (fill_checker == "1"):
+                                # Box is occupied. Try again
+                                print("Place already filled. Try again!!")
+                                continue
+                            elif (fill_checker == "0"): 
+                                # Everything in order
+                                break
+                            else: 
+                                # Should never run. DEBUGGING
+                                input("ERROR 2. Please Exit")
+                    except(ValueError):
+                        print("Please input a number (1-9)")
+                        continue
+            else:
+                # Should never run. DEBUGGING
+                input("ERROR 1. Please Exit")
+                
+            # Check if we should continue
+            continue_checker = clientSocket.recv(BUFFER_SIZE).decode()
+            
+            if (continue_checker == "Continue"):
+                continue
+            elif (continue_checker == "Finish"):
+                curr_board = clientSocket.recv(BUFFER_SIZE).decode()
+                print(curr_board)
+                game_message = clientSocket.recv(BUFFER_SIZE).decode()
+                print(game_message)
+                break
+        
+        # Receive scoreboard
+        curr_scoreboard = clientSocket.recv(BUFFER_SIZE).decode()
+        print(curr_scoreboard)
     
-except IOError:
-    print("File was not found!")
+        
+    
 except Exception:
     print("Error!")
-    print(Exception + "\n")
+    print(str(Exception) + "\n")
 finally:
     input("Press Enter to continue") # Here to wait for user input before closing program
-    file.close()
     clientSocket.close()
--- server.py
+++ server.py
@@ -1,32 +1,18 @@
 # Function to print Tic Tac Toe
 def print_tic_tac_toe(values):
-    print("\n")
-    print("\t     |     |")
-    print("\t  {}  |  {}  |  {}".format(values[0], values[1], values[2]))
-    print('\t_____|_____|_____')
- 
-    print("\t     |     |")
-    print("\t  {}  |  {}  |  {}".format(values[3], values[4], values[5]))
-    print('\t_____|_____|_____')
- 
-    print("\t     |     |")
- 
-    print("\t  {}  |  {}  |  {}".format(values[6], values[7], values[8]))
-    print("\t     |     |")
-    print("\n")
+    curr_board = "\n" + "\t     |     |" + "\n\t  {}  |  {}  |  {}".format(values[0], values[1], values[2]) + "\n\t_____|_____|_____" + "\n\t     |     |" + "\n\t  {}  |  {}  |  {}".format(values[3], values[4], values[5]) + "\nt_____|_____|_____" + "\n\t     |     |" + "\n\t  {}  |  {}  |  {}".format(values[6], values[7], values[8]) + "\n\t     |     |" + "\n"
+    return curr_board
  
  
 # Function to print the score-board
 def print_scoreboard(score_board):
-    print("\t--------------------------------")
-    print("\t              SCOREBOARD       ")
-    print("\t--------------------------------")
+    curr_scoreboard = "\t--------------------------------" + "\n\t SCOREBOARD       " + "\n\t--------------------------------\n"
  
     players = list(score_board.keys())
-    print("\t   ", players[0], "\t    ", score_board[players[0]])
-    print("\t   ", players[1], "\t    ", score_board[players[1]])
- 
-    print("\t--------------------------------\n")
+    curr_scoreboard += ("\t   ", players[0], "\t    ", score_board[players[0]] + "\n")
+    curr_scoreboard += ("\t   ", players[1], "\t    ", score_board[players[1]] + "\n")
+    curr_scoreboard += ("\t--------------------------------\n")
+    return curr_scoreboard
  
 # Function to check if any player has won
 def check_win(player_pos, cur_player):
@@ -50,7 +36,7 @@ def check_draw(player_pos):
     return False       
  
 # Function for a single game of Tic Tac Toe
-def single_game(cur_player):
+def single_game(piece_letter):
  
     # Represents the Tic Tac Toe
     values = [' ' for x in range(9)]
@@ -60,65 +46,77 @@ def single_game(cur_player):
      
     # Game Loop for a single game of Tic Tac Toe
     while True:
-        print_tic_tac_toe(values)
-         
-        # Try exception block for MOVE input
-        try:
-            print("Player ", cur_player, " turn. Which box? : ", end="")
-            move = int(input()) 
-        except ValueError:
-            print("Wrong Input!!! Try Again")
-            continue
+        # Send current boards 
+        for socket in sockets:
+            socket.sendall(print_tic_tac_toe(values).encode())
+        
+        # Send 1 or 0 depending on who's turn it is (1 for yes, 0 for no)
+        for socket in sockets:
+            if (socket == sockets[curr_socket]):
+                socket.sendall("1".encode())
+            else:
+                socket.sendall("0".encode())
+        
+        sockets[curr_socket].sendall("Your turn! Which box : ".encode())
+        move = int(sockets[curr_socket].recv(BUFFER_SIZE).decode())
  
-        # Sanity check for MOVE inout
-        if move < 1 or move > 9:
-            print("Wrong Input!!! Try Again")
-            continue
- 
-        # Check if the box is not occupied already
+        # Check if the box is not occupied already (1 for yes, 0 for no)
         if values[move-1] != ' ':
-            print("Place already filled. Try again!!")
+            sockets[curr_socket].sendall("1".encode())
             continue
+        else:
+            sockets[curr_socket].sendall("0".encode())
  
         # Update game information
  
         # Updating grid status 
-        values[move-1] = cur_player
+        values[move-1] = piece_letter
  
         # Updating player positions
-        player_pos[cur_player].append(move)
+        player_pos[piece_letter].append(move)
  
         # Function call for checking win
-        if check_win(player_pos, cur_player):
-            print_tic_tac_toe(values)
-            print("Player ", cur_player, " has won the game!!")     
-            print("\n")
-            return cur_player
- 
+        if check_win(player_pos, piece_letter):
+            for socket in sockets:
+                # Return a "finish" message (to know that the game is over)
+                socket.sendall("Finish".encode())
+                # Send Won game
+                socket.sendall(print_tic_tac_toe(values).encode())
+                socket.sendall(("Player " + piece_letter + " has won the game!!").encode())   
+            return piece_letter
         # Function call for checking draw game
-        if check_draw(player_pos):
-            print_tic_tac_toe(values)
-            print("Game Drawn")
-            print("\n")
+        elif check_draw(player_pos):
+            for socket in sockets:
+                # Return a "finish" message (to know that the game is over)
+                socket.sendall("Finish".encode())
+                # Send Drawn game
+                socket.sendall(print_tic_tac_toe(values).encode())
+                socket.sendall("Game Drawn".encode())
             return 'D'
+        # Return a "continue" message (to know that we continue)
+        else:
+            for socket in sockets:
+                socket.sendall("Continue".encode())
  
         # Switch player moves
-        if cur_player == 'X':
-            cur_player = 'O'
+        if piece_letter == 'X':
+            piece_letter = 'O'
+            curr_socket = (curr_socket + 1) % 2
         else:
-            cur_player = 'X'
+            piece_letter = 'X'
+            curr_socket = (curr_socket + 1) % 2
  
 from socket import *
 
 # Server IP Address and Port
-HOST = "18.219.39.168"
+HOST = "localhost"
 PORT = 1200
 BUFFER_SIZE = 1024
 
 # Setting up our socket
 serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind((HOST, PORT))
-serverSocket.listen(2)
+serverSocket.bind(("", PORT))
+serverSocket.listen()
 
 while True:
     try:
@@ -145,132 +143,51 @@ while True:
         # Stores the options 
         options = ["X", "O"]
         
+        # May change
+        player_choice["X"] = player1
+        player_choice["O"] = player2
+        
         # Stores the scoreboard
         score_board = {player1 : 0, player2: 0}
         
+        # Stores the sockets
+        sockets = [connectionSocket1, connectionSocket2]
+        
+        # Stores the current socket index (for the current player)
+        curr_socket = 0
+        
         # Send current scoreboard
-        """
-        connectionSocket1.sendall(print_scoreboard(score_board).encode())
-        connectionSocket2.sendall(print_scoreboard(score_board).encode())
-        """
+        for socket in sockets:
+            socket.sendall(print_scoreboard(score_board).encode())
         
         # Game Loop for a series of Tic Tac Toe
         # The loop runs until the players quit 
         while True:
-     
-            # Player choice Menu
-            if (cur_player == player1):
-                currentState = "Turn to choose for" + cur_player + "\n"
-                                "Enter 1 for X \n" +
-                                "Enter 2 for O \n" +
-                                "Enter 3 to Quit \n"
-                                
-                # Try exception for CHOICE input
-                while True:
-                    connectionSocket1.sendall(currentState.encode())
-                    choice = int(connectionSocket1.recv(BUFFER_SIZE).decode())
-                    if choice >= 1 and choice <= 3:
-                        break
-                    else 
-                        # ERROR MESSAGE
-         
-                # Conditions for player choice  
-                if choice == 1:
-                    player_choice['X'] = cur_player
-                    if cur_player == player1:
-                        player_choice['O'] = player2
-                    else:
-                        player_choice['O'] = player1
-         
-                elif choice == 2:
-                    player_choice['O'] = cur_player
-                    if cur_player == player1:
-                        player_choice['X'] = player2
-                    else:
-                        player_choice['X'] = player1
-                 
-                elif choice == 3:
-                    # TBD                    
-                    """
-                    print("Final Scores")
-                    print_scoreboard(score_board)
-                    break  
-                    """
-         
-                # Stores the winner in a single game of Tic Tac Toe
-                winner = single_game(options[choice-1])
-                 
-                # Edits the scoreboard according to the winner
-                if winner != 'D' :
-                    player_won = player_choice[winner]
-                    score_board[player_won] = score_board[player_won] + 1
-         
-                connectionSocket1.sendall(print_scoreboard(score_board).encode())
-                connectionSocket2.sendall(print_scoreboard(score_board).encode())
-                # Switch player who chooses X or O
-                if cur_player == player1:
-                    cur_player = player2
-                else:
-                    cur_player = player1
+            # Stores the winner in a single game of Tic Tac Toe
+            winner = single_game(options[curr_socket])
+             
+            # Edits the scoreboard according to the winner
+            if winner != 'D' :
+                player_won = player_choice[winner]
+                score_board[player_won] = score_board[player_won] + 1
+            
+            for socket in sockets:
+                socket.sendall(print_scoreboard(score_board).encode())
+                
+            # Switch player who chooses X or O
+            if cur_player == player1:
+                cur_player = player2
+                curr_socket = 1
             else:
-                currentState = "Turn to choose for" + cur_player + "\n"
-                                "Enter 1 for X \n" +
-                                "Enter 2 for O \n" +
-                                "Enter 3 to Quit \n"
-                                
-                # Try exception for CHOICE input
-                while True:
-                    connectionSocket2.sendall(currentState.encode())
-                    choice = int(connectionSocket2.recv(BUFFER_SIZE).decode())
-                    if choice >= 1 and choice <= 3:
-                        break
-                    else 
-                        # ERROR MESSAGE
-         
-                # Conditions for player choice  
-                if choice == 1:
-                    player_choice['X'] = cur_player
-                    if cur_player == player1:
-                        player_choice['O'] = player2
-                    else:
-                        player_choice['O'] = player1
-         
-                elif choice == 2:
-                    player_choice['O'] = cur_player
-                    if cur_player == player1:
-                        player_choice['X'] = player2
-                    else:
-                        player_choice['X'] = player1
-                 
-                elif choice == 3:
-                    # TBD                    
-                    """
-                    print("Final Scores")
-                    print_scoreboard(score_board)
-                    break  
-                    """
-         
-                # Stores the winner in a single game of Tic Tac Toe
-                winner = single_game(options[choice-1])
-                 
-                # Edits the scoreboard according to the winner
-                if winner != 'D' :
-                    player_won = player_choice[winner]
-                    score_board[player_won] = score_board[player_won] + 1
-         
-                connectionSocket1.sendall(print_scoreboard(score_board).encode())
-                connectionSocket2.sendall(print_scoreboard(score_board).encode())
-                # Switch player who chooses X or O
-                if cur_player == player1:
-                    cur_player = player2
-                else:
-                    cur_player = player1
+                cur_player = player1
+                curr_socket = 0
     except Exception:
         print("Error!")
         print(Exception + "\n")
     finally:
         # Close connection with client
-        connectionSocket.close()
+        for socket in sockets:
+            socket.close()
         print("Connection successfully terminated!\n")
         
 
